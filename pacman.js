938e2c7dabdab686e3c71fab335658e61d7a40ef
Claire Zhang <43884811+claireyyz@users.noreply.github.com>
2022-08-24 17:36:40 -0400

    Update pacman.js

--- pacman.js
+++ pacman.js
@@ -1,46 +1,70 @@
-// pos is the PacMan image position variable- it is set to 0 initially
 var pos = 0;
-//pageWidth is the width of the webpage. This is later used to calculate when Pac-Man needs to turn around. 
-let pageWidth = window.innerWidth;
-//This array contains all the PacMan movement images
 const pacArray = [
   ['./images/pacman1.png', './images/pacman2.png'],
   ['./images/pacman3.png', './images/pacman4.png'],
 ];
-
-// this variable defines what direction should PacMan go into:
-// 0 = left to right
-// 1 = right to left (reverse)
 var direction = 0;
+const pacMen = [];
 
-// This variable helps determine which PacMan image should be displayed. It flips between values 0 and 1
-var focus = 0;
-
-// This function is called on mouse click. Every time it is called, it updates the PacMan image, position and direction on the screen.
-function Run() {
-  let img = document.getElementById('PacMan');
-  let imgWidth = img.width;
-  focus = (focus + 1) % 2;
-  direction = checkPageBounds(direction, imgWidth, pos, null);
-  img.src = pacArray[direction][focus];
-  if (direction) {
-    pos -= 20;
-    img.style.left = pos + 'px';
-  } else {
-    pos += 20;
-    img.style.left = pos + 'px';
-  }
+function setToRandom(scale) {
+  return {
+    x: Math.random() * scale,
+    y: Math.random() * scale,
+  };
+}
+// Factory to make a PacMan
+function makePac() {
+  // returns an object with values scaled {x: 33, y: 21}
+  let velocity = setToRandom(10);
+  let position = setToRandom(200);
+  // Add image to div id = game
+  let game = document.getElementById('game');
+  let newimg = document.createElement('img');
+  newimg.style.position = 'absolute';
+  newimg.src = './images/pacman1.png';
+  newimg.width = 100;
+  newimg.style.left = position.x;
+  newimg.style.top = position.y;
+  game.appendChild(newimg);
+  // new style of creating an object
+  return {
+    position,
+    velocity,
+    newimg,
+  };
 }
 
-// This function determines the direction of PacMan based on screen edge detection. This uses the setInterval method instead of the setTimeout method
+function update() {
+  //loop over pacmen array and move each one and move image in DOM
+  pacMen.forEach((item) => {
+    checkCollisions(item);
+    item.position.x += item.velocity.x;
+    item.position.y += item.velocity.y;
 
-function checkPageBounds(direction, imgWidth, pos, pageWidth) {
-  //
-  if (direction == 0 && pos + imgWidth >= pageWidth) direction = 1;
-  if (direction == 1 && pos < 0) direction = 0;
-  //
-  return direction;
+    item.newimg.style.left = item.position.x;
+    item.newimg.style.top = item.position.y;
+  });
+  setTimeout(update, 20);
 }
-setInterval(Run, 200);
-//Please do not change
-module.exports = checkPageBounds;
+
+function checkCollisions(item) {
+  if (
+    item.position.x + item.velocity.x + item.newimg.width > window.innerWidth ||
+    item.position.x + item.velocity.x < 0
+  )
+    item.velocity.x = -item.velocity.x;
+  if (
+    item.position.y + item.velocity.y + item.newimg.height > window.innerHeight ||
+    item.position.y + item.velocity.y < 0
+  )
+    item.velocity.y = -item.velocity.y;
+}
+
+function makeOne() {
+  pacMen.push(makePac()); // add a new PacMan
+}
+
+//don't change this line
+if (typeof module !== 'undefined') {
+  module.exports = { checkCollisions, update, pacMen };
+}
